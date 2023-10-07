c01620798bf049eff960b6ea2e2127cb0ce51b5f
Serkan Konakcı <serkankonakci@gmail.com>
2023-10-08 00:46:12 +0300

    feat(module): auto-load components

--- modules/@timus/table/components/export.js
+++ modules/@timus/table/components/export.js
@@ -1,3 +1,19 @@
-import TableCreate from './TableCreate'
+const components = {}
+const requireComponent = require.context(
+  '.', // bu klasördeki bileşenleri oku
+  false, // alt klasörleri okuma
+  /[A-Z]\w+\.(vue)$/, // Vue bileşenlerini veya JS dosyalarını al
+  // EXPERIMENTAL: /[A-Z]\w+\.(vue|js)$/, // Vue bileşenlerini veya JS dosyalarını al
+)
 
-export default { TableCreate }
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName)
+  const componentName = fileName
+    .split('/')
+    .pop()
+    .replace(/\.\w+$/, '') // Dosya adını bileşen adı olarak al
+
+  components[componentName] = componentConfig.default || componentConfig
+})
+
+export default components
