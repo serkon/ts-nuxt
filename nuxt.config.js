fcfa38feb59856194066200bd54bff3d8ad123e2
Serkan Konakcı <serkankonakci@gmail.com>
2023-10-08 11:00:10 +0300

    feat(plugin): add titlecase directive to plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   css: ['element-ui/lib/theme-chalk/index.css', '@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui'],
+  plugins: ['@/plugins/element-ui', '@/plugins/case-converter'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
