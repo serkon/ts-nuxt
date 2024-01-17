b148a73f7c07075ff74580f1481e2435304eea11
serkon <serkons@yahoo.com>
2024-01-17 20:05:49 +0300

    refactor(theme): release v.1.0.65

--- modules/@timus-networks/theme/components-js/ThemeCollapse.vue
+++ modules/@timus-networks/theme/components-js/ThemeCollapse.vue
@@ -5,37 +5,34 @@
       <p class="p-lg my-6">
         Use the type attribute to define Tag's type. In addition, the color attribute can be used to set the background color of the Tag.
       </p>
-      <div class="grid grid-flow-col auto-cols-max gap-4">
-        <el-collapse v-model="activeNames" @change="handleChange">
-          <el-collapse-item title="Consistency" name="1">
-            <div>
-              Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used
-              to;
-            </div>
-            <div>
-              Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.
-            </div>
-          </el-collapse-item>
-          <el-collapse-item title="Feedback" name="2">
-            <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
-            <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
-          </el-collapse-item>
-          <el-collapse-item title="Efficiency" name="3">
-            <div>Simplify the process: keep operating process simple and intuitive;</div>
-            <div>Definite and clear: enunciate your intentions clearly so that the users can quickly understand and make decisions;</div>
-            <div>
-              Easy to identify: the interface should be straightforward, which helps the users to identify and frees them from memorizing and
-              recalling.
-            </div>
-          </el-collapse-item>
-          <el-collapse-item title="Controllability" name="4">
-            <div>Decision making: giving advices about operations is acceptable, but do not make decisions for the users;</div>
-            <div>
-              Controlled consequences: users should be granted the freedom to operate, including canceling, aborting or terminating current operation.
-            </div>
-          </el-collapse-item>
-        </el-collapse>
-      </div>
+      <el-collapse v-model="activeNames" @change="handleChange">
+        <el-collapse-item title="Consistency" name="1">
+          <div>
+            Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used
+            to;
+          </div>
+          <div>
+            Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="Feedback" name="2">
+          <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
+          <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
+        </el-collapse-item>
+        <el-collapse-item title="Efficiency" name="3">
+          <div>Simplify the process: keep operating process simple and intuitive;</div>
+          <div>Definite and clear: enunciate your intentions clearly so that the users can quickly understand and make decisions;</div>
+          <div>
+            Easy to identify: the interface should be straightforward, which helps the users to identify and frees them from memorizing and recalling.
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="Controllability" name="4">
+          <div>Decision making: giving advices about operations is acceptable, but do not make decisions for the users;</div>
+          <div>
+            Controlled consequences: users should be granted the freedom to operate, including canceling, aborting or terminating current operation.
+          </div>
+        </el-collapse-item>
+      </el-collapse>
       <div class="my-4 p-4 border-l-4 border-blue-600 bg-blue-100">
         <p class="text-xs">
           <code>&lt;el-input placeholder="Please input" v-model="input"&gt;&lt;/el-input&gt;</code>
