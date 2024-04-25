cb30d8992c935ab5626eacf695788c9f9668bff8
serkon <serkons@yahoo.com>
2024-04-25 18:38:09 +0300

    feat(theme): update breadcrumbs design according to guide

--- modules/@timus-networks/theme/components-ts/TimusSamples.vue
+++ modules/@timus-networks/theme/components-ts/TimusSamples.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="container">
     <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="Button" name="first"><ThemeButtons></ThemeButtons></el-tab-pane>
-      <el-tab-pane label="Typo" name="second"><ThemeTypo></ThemeTypo></el-tab-pane>
-      <el-tab-pane label="Input" name="third"><ThemeInputs></ThemeInputs></el-tab-pane>
-      <el-tab-pane label="Checkbox" name="fourth"><ThemeCheckbox></ThemeCheckbox></el-tab-pane>
-      <el-tab-pane label="Radio" name="five"><ThemeRadio></ThemeRadio></el-tab-pane>
-      <el-tab-pane label="Number" name="six"><ThemeInputNumbers></ThemeInputNumbers></el-tab-pane>
-      <el-tab-pane label="Popover & Dropdown" name="seven"><ThemePopover></ThemePopover></el-tab-pane>
-      <el-tab-pane label="Select" name="eight"><ThemeSelect></ThemeSelect></el-tab-pane>
-      <el-tab-pane label="Form" name="nine"><ThemeForm></ThemeForm></el-tab-pane>
-      <el-tab-pane label="Link" name="ten"><ThemeLink></ThemeLink></el-tab-pane>
-      <el-tab-pane label="Tag" name="eleven"><ThemeTag></ThemeTag></el-tab-pane>
-      <el-tab-pane label="Switch" name="twelve"><ThemeToggle></ThemeToggle></el-tab-pane>
-      <el-tab-pane label="Table" name="thirteen"><ThemeTable></ThemeTable></el-tab-pane>
-      <el-tab-pane label="Tooltip" name="fourteen"><ThemeTooltip></ThemeTooltip></el-tab-pane>
-      <el-tab-pane label="Alert" name="fifteen"><ThemeAlert></ThemeAlert></el-tab-pane>
-      <el-tab-pane label="Cascader" name="sixteen"><ThemeCascader></ThemeCascader></el-tab-pane>
-      <el-tab-pane label="Badge" name="seventeen"><ThemeBadge></ThemeBadge></el-tab-pane>
-      <el-tab-pane label="Avatar" name="eighteen"><ThemeAvatar></ThemeAvatar></el-tab-pane>
-      <el-tab-pane label="Logo" name="nineteen"><ThemeLogo></ThemeLogo></el-tab-pane>
-      <el-tab-pane label="Dialog" name="twenty"><ThemeDialog></ThemeDialog></el-tab-pane>
-      <el-tab-pane label="Sidebar" name="twenty-one"><ThemeSidebar></ThemeSidebar></el-tab-pane>
-      <el-tab-pane label="Upload" name="twenty-two"><ThemeUpload></ThemeUpload></el-tab-pane>
-      <el-tab-pane label="MessageBox" name="twenty-three"><ThemeMessageBox></ThemeMessageBox></el-tab-pane>
-      <el-tab-pane label="Information" name="twenty-four"><ThemeInformation></ThemeInformation></el-tab-pane>
-      <el-tab-pane label="Collapse" name="twenty-five"><ThemeCollapse></ThemeCollapse></el-tab-pane>
+      <el-tab-pane label="Button" name="first"><ThemeButtons /></el-tab-pane>
+      <el-tab-pane label="Breadcrumb" name="twenty-six"><ThemeBreadcrumb /></el-tab-pane>
+      <el-tab-pane label="Typo" name="second"><ThemeTypo /></el-tab-pane>
+      <el-tab-pane label="Input" name="third"><ThemeInputs /></el-tab-pane>
+      <el-tab-pane label="Checkbox" name="fourth"><ThemeCheckbox /></el-tab-pane>
+      <el-tab-pane label="Radio" name="five"><ThemeRadio /></el-tab-pane>
+      <el-tab-pane label="Number" name="six"><ThemeInputNumbers /></el-tab-pane>
+      <el-tab-pane label="Popover & Dropdown" name="seven"><ThemePopover /></el-tab-pane>
+      <el-tab-pane label="Select" name="eight"><ThemeSelect /></el-tab-pane>
+      <el-tab-pane label="Form" name="nine"><ThemeForm /></el-tab-pane>
+      <el-tab-pane label="Link" name="ten"><ThemeLink /></el-tab-pane>
+      <el-tab-pane label="Tag" name="eleven"><ThemeTag /></el-tab-pane>
+      <el-tab-pane label="Switch" name="twelve"><ThemeToggle /></el-tab-pane>
+      <el-tab-pane label="Table" name="thirteen"><ThemeTable /></el-tab-pane>
+      <el-tab-pane label="Tooltip" name="fourteen"><ThemeTooltip /></el-tab-pane>
+      <el-tab-pane label="Alert" name="fifteen"><ThemeAlert /></el-tab-pane>
+      <el-tab-pane label="Cascader" name="sixteen"><ThemeCascader /></el-tab-pane>
+      <el-tab-pane label="Badge" name="seventeen"><ThemeBadge /></el-tab-pane>
+      <el-tab-pane label="Avatar" name="eighteen"><ThemeAvatar /></el-tab-pane>
+      <el-tab-pane label="Logo" name="nineteen"><ThemeLogo /></el-tab-pane>
+      <el-tab-pane label="Dialog" name="twenty"><ThemeDialog /></el-tab-pane>
+      <el-tab-pane label="Sidebar" name="twenty-one"><ThemeSidebar /></el-tab-pane>
+      <el-tab-pane label="Upload" name="twenty-two"><ThemeUpload /></el-tab-pane>
+      <el-tab-pane label="MessageBox" name="twenty-three"><ThemeMessageBox /></el-tab-pane>
+      <el-tab-pane label="Information" name="twenty-four"><ThemeInformation /></el-tab-pane>
+      <el-tab-pane label="Collapse" name="twenty-five"><ThemeCollapse /></el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -35,6 +36,7 @@ import ThemeInputs from './ThemeInputs.vue';
 import ThemePopover from './ThemePopover.vue';
 
 export default {
+  components: { ThemeInputs, ThemeInputNumbers, ThemePopover },
   data() {
     return {
       activeName: 'fifteen',
@@ -45,6 +47,5 @@ export default {
       console.log(tab, event);
     },
   },
-  components: { ThemeInputs, ThemeInputNumbers, ThemePopover },
 };
 </script>
